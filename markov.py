72519b61de39d602b70a9c4a33f353314a6f1a9c
Paulo Sell <sellpaulof@gmail.com>
2019-08-25 18:52:53 -0300

    markov

--- markov.py
+++ markov.py
@@ -1,16 +1,27 @@
 import numpy as np
 import random
 
-def PMFdata(states, Pi):
-	u = np.random()
-    
+def PMFdata(N,xi,pX):
+    bi = []
+    x = 0
+    M = len(xi)
+    for k in range(M-1):        
+        if k == 0:
+            bi.append(pX[k])
+        else:
+            bi.append(bi[k-1]+pX[k])  
+    u = np.random.random()
+    if (u > 0) and u <= bi[0]:
+        x = xi[0]
+    for k in range(1,M):
+        if u > bi[k-1] and u <= bi[k]:
+            x = xi[k]
+    return x
 
+N = 1000000
+p0 = [1,0,0,0,0,0,0,0,0,0]
 
-N = 100000
-
-initial_state = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-P = np.matrix([[0.2, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+P = [[0.2, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
 			  [0.0, 0.2, 0.3, 0.3, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0],
 			  [0.0, 0.0, 0.1, 0.0, 0.0, 0.9, 0.0, 0.0, 0.0, 0.0],
 			  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
@@ -19,35 +30,21 @@ P = np.matrix([[0.2, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
 			  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.2],
 		 	  [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
 			  [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-			  [0.2, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2]])
+			  [0.2, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2]]
 
-states = np.arange(10)+1
-state = 1
-a = 0
-for i in range(N):
-	dado = round(random.uniform(0,1), 1)
-	print((dado))
-	if state == 1:
-		print(state)
-	elif state == 2:
-		print(state)
-	elif state == 3:
-		print(state)
-	elif state == 4:
-		print(state)
+xi = [1,2,3,4,5,6,7,8,9,10,11]
+XO = PMFdata(1,xi,p0)
+i = XO 
+X = []
+X.append(PMFdata(1,xi,P[i]))
+i = X[0]+1
+for n in range(1,N-1):
+    i = X[n-1]   -1 
+    X.append(PMFdata(1,xi,P[i]))
 
-	elif state == 5:
-		print(state)	
-	elif state == 6:
-		print(state)	
-	elif state == 7:
-		print(state)
-	elif state == 8:
-		print(state)
-	elif state == 9:
-		print(state)	
-	elif state == 10:
-		print(state)	
-	
+estados = [0,0,0,0,0,0,0,0,0,0]
 
-print(np.random()) 
+for estado in X:
+    estados[estado-1] = estados[estado-1] + 1
+
+estados = np.array(estados)
